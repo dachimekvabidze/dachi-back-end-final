7df0b4897f546d2b533d278cd33782c1d3a95b1e
Dachi Mekvabidze <114247817+dachimekvabidze@users.noreply.github.com>
2025-01-21 16:57:27 +0400

    Add files via upload

---
+++ back end final/models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    contents = db.relationship('Content', backref='author', lazy=True)
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
